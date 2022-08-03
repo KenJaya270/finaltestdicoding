a703733ae03c952fce79cdc8f11b8ef6fdfa5142
KenJaya270 <kenjayakusuma123@gmail.com>
2022-08-03 11:19:08 +0800

    finishing

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ const burgerIcon = document.querySelector("nav .burger-icon-container input");
 
 const nav = document.querySelector('nav ul');
 
-burgerIcon.addEventListener("click", function(){
-    nav.classList.toggle('slider');
-});
+// burgerIcon.addEventListener("click", function(){
+//     nav.classList.toggle('slider');
+// });
 
 const xIcon = document.querySelector('.x-container input');
+const sideBar = document.getElementById("sideBar");
+
+let clicked = 0;
+
+function sideBarOperation(){
+    if(clicked == 0){
+        sideBar.style.right = 0;
+        sideBar.style.transition = "0.3s";
+        clicked = 1;
+    } else if(clicked == 1){
+        sideBar.style.right = "-1000px";
+        sideBar.style.transition = "0.3s";
+        clicked = 0;
+    }
+}
+
+
+
 
